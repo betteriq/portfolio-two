301479abcdfdf5e6aacacfe978ed9264225b37f8
betteriq <betteriq@yahoo.com>
2022-04-16 13:17:18 +0430

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import Navbar from "./Components/Navbar";
 function App() {
-  return <div className="container">App</div>;
+  return (
+    <div className="container">
+      <Navbar /> 
+      <div className="container_text">
+
+      <h1>Web Developer , Designer & Programmer</h1>
+      <p>" Turning bright ideas into beautiful, useful, and delightful digital products everyone can use "</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css"
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav_main">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
